71eb1c38df1984bf195bc335d0cd1ccbdfd242f8
onthefirstday <56761682+onthefirstday@users.noreply.github.com>
2025-03-11 11:14:35 +0100

    Add files via upload

---
+++ RDW_API_test.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sodapy import Socrata
+import streamlit as st
+
+
+DOMAIN = "opendata.rdw.nl"
+DATASET_ID_basis = "m9d7-ebf2"
+DATASET_ID_fuel = "8ys7-d773"
+
+# Initialize Socrata client (public datasets don't require authentication)
+client = Socrata(DOMAIN, None)
+
+# Define specific columns to fetch
+options = st.multiselect(
+    "Which columns do you want to load?",
+    ["kenteken, brandstof_omschrijving, co2_uitstoot_gecombineerd"],
+)
+
+# Fetch data with selected columns
+results = client.get(DATASET_ID_fuel, select=options, limit=10000)
+
+df_fuel = pd.DataFrame.from_records(results)
+
+df_fuel.head()
+
+
